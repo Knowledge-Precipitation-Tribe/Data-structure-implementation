eec5fd3efc250301e231788c350ac314bffccabb
super <supeng842499467@gmail.com>
2020-05-04 22:28:06 +0800

    add stack

--- go-implementation/stack/sequential-stack/Stack.go
+++ go-implementation/stack/sequential-stack/Stack.go
@@ -14,6 +14,8 @@ func (stack *Stack)InitStack(){
 	stack.top = -1
 }
 
+
+//判断是否为空
 func (stack *Stack)IsEmpty() bool{
 	if stack.top == -1{
 		return true
@@ -21,11 +23,13 @@ func (stack *Stack)IsEmpty() bool{
 	return false
 }
 
+//数据入栈
 func (stack *Stack)Push(v interface{}){
 	stack.top++
 	stack.data = append(stack.data, v)
 }
 
+//数据出栈
 func (stack *Stack)Pop() (interface{}, error){
 	if stack.IsEmpty(){
 		return nil, errors.New("empty stack")
