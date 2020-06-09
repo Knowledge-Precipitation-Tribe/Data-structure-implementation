c71d1939de9dc7eda3dfe2050ef3573468a66cb1
super <supeng842499467@gmail.com>
2020-06-09 10:46:05 +0800

    sort

--- go-implementation/Sorting-algorithm/main.go
+++ go-implementation/Sorting-algorithm/main.go
@@ -6,9 +6,10 @@ import (
 )
 
 func main() {
-	arr := []int{5,8,7,9,10,3,2,1}
+	arr := []int{5, 8, 7, 9, 10, 3, 2, 1, 4}
 	//arr = sort_algo.BubbleSort(arr)
 	//arr = sort_algo.SelectSort(arr)
-	arr = sort_algo.InsertionSort(arr)
+	//arr = sort_algo.InsertionSort(arr)
+	sort_algo.MergeSort(arr, 0, len(arr) - 1)
 	fmt.Println(arr)
 }
---
+++ go-implementation/Sorting-algorithm/sort-algo/Merge-sort.go
@@ -0,0 +1,47 @@
+package sort_algo
+
+
+func MergeSort(arr []int, l int, r int) {
+	if l >= r {
+		return
+	}
+
+	// 递归向下
+	mid := (r + l) / 2
+	MergeSort(arr, l, mid)
+	MergeSort(arr, mid+1, r)
+	// 归并向上
+	merge(arr, l, mid, r)
+}
+
+// 合并 [l,r] 两部分数据，mid 左半部分的终点，mid + 1 是右半部分的起点
+func merge(arr []int, l int, mid int, r int) {
+	// 因为需要直接修改 arr 数据，这里首先复制 [l,r] 的数据到新的数组中，用于赋值操作
+	temp := make([]int, r-l+1)
+	for i := l; i <= r; i++ {
+		temp[i-l] = arr[i]
+	}
+
+	// 指向两部分起点
+	left := l
+	right := mid + 1
+
+	for i := l; i <= r; i++ {
+		// 左边的点超过中点，说明只剩右边的数据
+		if left > mid {
+			arr[i] = temp[right-l]
+			right++
+			// 右边的数据超过终点，说明只剩左边的数据
+		} else if right > r {
+			arr[i] = temp[left-l]
+			left++
+			// 左边的数据大于右边的数据，选小的数字
+		} else if temp[left - l] > temp[right - l] {
+			arr[i] = temp[right - l]
+			right++
+		} else {
+			arr[i] = temp[left - l]
+			left++
+		}
+	}
+}
