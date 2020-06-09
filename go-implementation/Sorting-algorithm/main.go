3aa8346051707fc18c45246b908b9568b118dee3
super <supeng842499467@gmail.com>
2020-06-09 09:46:17 +0800

    sort

--- go-implementation/Sorting-algorithm/main.go
+++ go-implementation/Sorting-algorithm/main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	arr := []int{5,8,7,9,10,3,2,1}
 	//arr = sort_algo.BubbleSort(arr)
-	arr = sort_algo.SelectSort(arr)
+	//arr = sort_algo.SelectSort(arr)
+	arr = sort_algo.InsertionSort(arr)
 	fmt.Println(arr)
 }
--- go-implementation/Sorting-algorithm/sort-algo/Insert-sort.go
+++ go-implementation/Sorting-algorithm/sort-algo/Insert-sort.go
@@ -1,2 +1,17 @@
 package sort_algo
 
+func InsertionSort(arr []int) []int {
+	length := len(arr)
+	var preIndex int
+	var current int
+	for i := 1; i < length; i++ {
+		preIndex = i - 1
+		current = arr[i]
+		for preIndex >= 0 && arr[preIndex] > current {
+			arr[preIndex+1] = arr[preIndex]
+			preIndex = preIndex - 1
+		}
+		arr[preIndex+1] = current
+	}
+	return arr
+}
