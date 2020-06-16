887e4845fb6561a33d11b7224eba6627dde1ef75
super <supeng842499467@gmail.com>
2020-06-16 10:27:38 +0800

    search

--- go-implementation/Search/algo/BinarySearch.go
+++ go-implementation/Search/algo/BinarySearch.go
@@ -102,3 +102,23 @@ func BSearchFirstBigger(array []int, n int, value int) int {
 	}
 	return -1
 }
+
+//查找最后一个小于等于给定值的元素
+func BSearchLastSmaller(array []int, n int, value int) int {
+	low := 0
+	high := n - 1
+
+	for low <= high{
+		mid := (low + high) / 2
+		if array[mid] > value{
+			high = mid -1
+		}else {
+			if mid == n - 1 || array[mid + 1] > value{
+				return mid
+			}else {
+				low = mid + 1
+			}
+		}
+	}
+	return -1
+}
--- go-implementation/Search/main.go
+++ go-implementation/Search/main.go
@@ -6,10 +6,11 @@ import (
 )
 
 func main() {
-	array := []int{3,4,6,7,10}
+	array := []int{3,5,6,8,9,10}
 	//fmt.Println(algo.BsearchRecu(array, 9, 4))
 	//fmt.Println(algo.BSearch(array, 9, 4))
 	//fmt.Println(algo.BSearchFirst(array, 10, 8))
 	//fmt.Println(algo.BSearchLast(array, 10, 8))
-	fmt.Println(algo.BSearchFirstBigger(array, 5, 5))
+	//fmt.Println(algo.BSearchFirstBigger(array, 6, 7))
+	fmt.Println(algo.BSearchLastSmaller(array, 6, 7))
 }
