50efb358ea449f09a1323892fdb9bcefef0fe5a2
super <supeng842499467@gmail.com>
2020-06-16 10:19:29 +0800

    search

--- go-implementation/Search/algo/BinarySearch.go
+++ go-implementation/Search/algo/BinarySearch.go
@@ -60,3 +60,45 @@ func BSearchFirst(array []int, n int, value int) int {
 	}
 	return -1
 }
+
+//查找第一个值等于给定值的元素
+func BSearchLast(array []int, n int, value int) int {
+	low := 0
+	high := n - 1
+
+	for low <= high{
+		mid := (low + high) / 2
+		if array[mid] < value{
+			low = mid + 1
+		}else if array[mid] > value{
+			high = mid - 1
+		}else{
+			if mid == n - 1 || array[mid + 1] != value{
+				return mid
+			}else {
+				low = mid + 1
+			}
+		}
+	}
+	return -1
+}
+
+//查找第一个大于等于给定值的元素
+func BSearchFirstBigger(array []int, n int, value int) int {
+	low := 0
+	high := n - 1
+
+	for low <= high{
+		mid := (low + high) / 2
+		if array[mid] >= value{
+			if mid == 0 || array[mid - 1] < value{
+				return mid
+			}else{
+				high= mid - 1
+			}
+		}else {
+			low = mid + 1
+		}
+	}
+	return -1
+}
--- go-implementation/Search/main.go
+++ go-implementation/Search/main.go
@@ -6,8 +6,10 @@ import (
 )
 
 func main() {
-	array := []int{1,3,4,5,6,8,8,8,11,18}
+	array := []int{3,4,6,7,10}
 	//fmt.Println(algo.BsearchRecu(array, 9, 4))
 	//fmt.Println(algo.BSearch(array, 9, 4))
-	fmt.Println(algo.BSearchFirst(array, 10, 8))
+	//fmt.Println(algo.BSearchFirst(array, 10, 8))
+	//fmt.Println(algo.BSearchLast(array, 10, 8))
+	fmt.Println(algo.BSearchFirstBigger(array, 5, 5))
 }
